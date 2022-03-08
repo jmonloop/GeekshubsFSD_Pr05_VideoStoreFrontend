9099f225cb17a23bc2d8aba481c6cb7a7c850055
jmonloop <monleonlopezjavier@gmail.com>
2022-03-08 20:06:03 +0100

    search logo added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,7 @@ import {connect} from 'react-redux';
 
 import './Header.css';
 import HambModal from '../HambModal/HambModal';
+import { Input } from '@mantine/core';
 
 const Header = (props) => {
 
@@ -35,6 +36,12 @@ const Header = (props) => {
     if(!props.credentials?.token){
         return (
             <div className='designHeader'>
+                <Input
+                    icon={<img className='icon' src={require('../../assets/icons/search.png')} />}
+                    placeholder="Search movie..."
+                    radius="xl"
+                    size="md"
+                />
                 <HambModal/>
             </div>
         )
