aa8fbc95a7a11ece411a111d5f327ada20530f82
jmonloop <monleonlopezjavier@gmail.com>
2022-03-08 10:23:33 +0100

    store comments added

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,15 +5,18 @@ import { save, load } from "redux-localstorage-simple";
 //Importo los reducers
 import reducer from './reducers';
 
-//Guardo estados que necesito almacenar. En este caso solo está 'credentials'
+
+
+//Guardo estados que necesito almacenar. En este caso solo está 'credentials' pero si queremos meter más, los metemos separados con comas
 const createStoreWithMiddleware = applyMiddleware(
 	save({ states: ['credentials'] })
 )(createStore);
 
-//Cargo estados que necesito actualizar. En este caso solo está 'credentials'
+//Cargo estados que necesito actualizar. En este caso solo está 'credentials' En este caso solo está 'credentials' pero si queremos meter más, los metemos separados con comas
 const store = createStoreWithMiddleware(
     reducer,
     load({ states: ['credentials'] }),
+    //Esta línea es para que funcione la extensión de Chrome de redux
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
         trace: true,
     })
