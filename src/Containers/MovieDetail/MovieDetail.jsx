28058af30a04b682b2f5dc9f87a6f43829f36dff
jmonloop <monleonlopezjavier@gmail.com>
2022-03-11 20:16:51 +0100

    detail genre id map added

--- src/Containers/MovieDetail/MovieDetail.jsx
+++ src/Containers/MovieDetail/MovieDetail.jsx
@@ -11,35 +11,121 @@ import * as S from './StMovieDetail.jsx';
 
 let res;
 
+
+
+
 const MovieDetail = (props) => {
     //vars
     let navigate = useNavigate();
     //hooks
+    const [genres, setGenres] = useState([props.search.genre_ids]);
 
-
+    console.log("genres",genres[0])
 
     //useEffects
     useEffect(() => {
         // console.log(props.search)
     }, []);
-    
+
     useEffect(() => {
     }, []);
-    
+
     useEffect(() => {
-        
+
     });
 
+    //funcs
+    const genresTMDB = (genre) => {
+        switch (genre) {
+            case 28:
+                return "Action";
+    
+            case 12:
+                return "Adventure";
+    
+            case 16:
+                return "Adventure";
+    
+            case 35:
+                return "Comedy";
+    
+            case 80:
+                return "Crime";
+    
+            case 99:
+                return "Documentary"
+    
+            case 18:
+                return "Drama";
+            
+            case 10751:
+                return "Family";
+    
+            case 14:
+                return "Fantasy";
+    
+            case 36:
+                return "History";
+    
+            case 27:
+                return "Horror";
+    
+            case 10402:
+                return "Music";
+    
+            case 9648:
+                return "Mysery";
+    
+            case 10749:
+                return "Romance";
+    
+            case 878:
+                return "Science Fiction";
+    
+            case 10770:
+                return "TV Movie";
+    
+            case 53:
+                return "Thriller";
+    
+            case 10752:
+                return "War";
+    
+            case 37:
+                return "Western"
+    
+            default:
+                break
+    
+        }
+    }
+    
 
     return (
-        <S.MovieDetailContainter>
-            <S.MovieDetailTitle>{props.search.original_title}</S.MovieDetailTitle>
-            <S.DetailsBox></S.DetailsBox>
+        <S.movieDetailContainter>
+            {/* <S.movieDetailTitle>{props.search.original_title}</S.movieDetailTitle> */}
+            <S.detailsBox>
+                <S.detailDiv>
+                    <S.detailKey>Original Title</S.detailKey>
+                    <S.detailValue>{props.search.original_title}</S.detailValue>
+                </S.detailDiv>
+                <S.detailDiv>
+                    <S.detailKey>Genres</S.detailKey>
+
+                            <S.detailValue>
+                                {genres[0].map(genre => {
+                                    return (<span>{genre}</span>)
+                                })}
+                            </S.detailValue>
+                        
+                </S.detailDiv>
+
+            </S.detailsBox>
 
 
 
 
-        </S.MovieDetailContainter>
+        </S.movieDetailContainter>
     )
 }
 
--- src/Containers/MovieDetail/StMovieDetail.jsx
+++ src/Containers/MovieDetail/StMovieDetail.jsx
@@ -1,7 +1,7 @@
 //Importo styled-components
 import styled from 'styled-components';
 //Declaro un nuevo elemento DIV llamado headerContainter. Para "pintarlo" en el jsx lo haremos con <S.headerContainer><S.headerContainer/>
-export const MovieDetailContainter = styled.div`
+export const movieDetailContainter = styled.div`
     background: rgb(6,126,68);
     background: linear-gradient(180deg, rgba(6,126,68,1) 2%, rgba(5,111,26,1) 28%, rgba(53,106,6,1) 92%);
 
@@ -14,18 +14,41 @@ export const MovieDetailContainter = styled.div`
     width: 100%;
 `
 
-export const MovieDetailTitle = styled.div`
-    height: 10%;
-    width: 50%;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 8vh;
-    margin-top: 3%;
-    margin-bottom: 3%;
-`
+export const detailsBox = styled.div`
 
-export const DetailsBox = styled.div`
+    margin-top: 5%;
     height: 100%;
     width: 80%;
 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
     background-color: gray;
+`
+
+export const detailDiv = styled.div`
+    height: 20%;
+    width: 80%;
+    
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+export const detailKey = styled.div`
+
+`
+
+export const detailValue = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const detailContent = styled.p`
+
 `
