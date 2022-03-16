0e8fd6b7db9dbcad4234fd0effe9f2187a451d5f
jmonloop <monleonlopezjavier@gmail.com>
2022-03-16 18:41:47 +0100

    search function added. Ready for debouncer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LOGOUT } from '../../redux/types';
 import { connect } from 'react-redux';
-
+import debounce from 'lodash.debounce';
 
 import HambModal from '../HambModal/HambModal';
 
@@ -11,10 +11,12 @@ import HambModal from '../HambModal/HambModal';
 //Importo todo lo que venga de HambModalSt. Lo llamaré S y lo que venga detrás del punto será el elemento creado en el styled
 import * as S from './StHeader';
 import MyTooltip from '../Tooltip/Tooltip';
+import axios from 'axios';
 
 const Header = (props) => {
 
     let navigate = useNavigate();
+    let filmsArr = [];
 
     //Hooks
     const [searchInput, setsearchInput] = useState("");
@@ -23,6 +25,7 @@ const Header = (props) => {
 
     useEffect(() => {
         console.log("actualizo search input a", searchInput)
+        console.log("actualizo filmsArr a ", filmsArr)
     },[searchInput])
 
     const goTo = (place) => {
@@ -46,6 +49,18 @@ const Header = (props) => {
 
     const quickSearch = (e) =>{
         setsearchInput(e.target.value)
+        findMovieByTitle(searchInput)
+    }
+
+    const findMovieByTitle = async (arg) => {
+        let result = [];
+        try {
+            result = await axios.get(`https://videostore-backend.herokuapp.com/films/title?arg=${arg}`)
+
+        } catch(error) {
+            console.log("Search movie by title error = ", error)
+        }
+        filmsArr = result.data.results;
     }
 
     return (
